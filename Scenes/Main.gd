b6c4dad3ac3bc0a20da5da945ac6b3dd2d67969a
Paulina Bartczak <paulinabartczak2@onet.pl>
2025-05-02 13:03:25 +0200

    Game finished.

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -14,13 +14,21 @@ func _process(delta):
 
 
 func game_over():
+	$Music.stop()
+	$Death.play()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
+	$Music.play()
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout():
@@ -51,6 +59,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
